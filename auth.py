08dfb8e9922f422515704346bdf045ad24b9db30
Akira <luckyanchor2@gmail.com>
2020-08-21 09:56:43 -0700

    I hate these messages

--- auth.py
+++ auth.py
@@ -8,4 +8,20 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/')
 def welcome():
-    return "Hello there"
+    return render_template('welcome.html')
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@auth.route('/signup')
+def signup_post():
+    return "Signup-post"
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+@auth.route('/login')
+def login_post():
+    return "Login-post"
