16706e40aa03d4ff17caa6efdca8322cf0ea024c
Akira <luckyanchor2@gmail.com>
2020-08-21 13:54:47 -0700

    working on profile page

--- profile_symptoms.py
+++ profile_symptoms.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, redirect, request
 from flask_login import login_required, current_user
 
 ps = Blueprint('ps', __name__)
 
-@ps.route('/profile')
+@ps.route('/profile', methods=['POST', 'GET'])
 @login_required
 def profile():
+    if current_user.role == 'Doctor' or 'Nurse':
+        redirect(url_for('dashboard.dashboard'))
+    print(request.form)
     return render_template('profile.html')
